4bb96ccdaea8d840b2dd87b43b042a1bcd240a87
m.giordano202 <m.giordano202@studenti.unisa.it>
2024-12-21 21:56:46 +0100

    Third commit: added images and a try-run

--- grid.py
+++ grid.py
@@ -1,20 +1,20 @@
 class Grid:
-    def __init__(self, row, cols):
-        self.row = row
+    def __init__(self, rows, cols):
+        self.rows = rows
         self.cols = cols
-        self.grid = [[0 for _ in range(cols)] for _ in range(row)]
+        self.grid = [[0 for _ in range(cols)] for _ in range(rows)]
 
-    def setCell(self, row, col, value):
-        self.grid[row][col] = value
+    def setCell(self, rows, cols, value):
+        self.grid[rows][cols] = value
 
-    def getCell(self, row, col):
-        return self.grid[row][col]
+    def getCell(self, rows, cols):
+        return self.grid[rows][cols]
 
-    def isFree(self, row, col):
-        return self.grid[row][col] == 0
+    def isFree(self, rows, cols):
+        return self.grid[rows][cols] == 0
 
     def printGrid(self, screen, images, cell_size):
-        for r in range(self.row):
+        for r in range(self.rows):
             for c in range(self.cols):
                 value = self.grid[r][c]
                 image = images.get(value, images["empty"])
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def main():
     pygame.init()
     game = BombermanGame(rows = 13, cols = 15, cell_size = 50)
     game.run()
+
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
