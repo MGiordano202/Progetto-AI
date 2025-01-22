6d8325216dbbcb197da3a64e46079e98cdab93df
MGiordano202 <114730112+MGiordano202@users.noreply.github.com>
2025-01-22 11:04:47 +0100

    18th: Defining a new type of individual as a string of [u, d, l, r, b]

--- AI/Genetic_Algorithm/individual.py
+++ AI/Genetic_Algorithm/individual.py
@@ -1,26 +1,27 @@
 import random
 
+from AI.Genetic_Algorithm.fitness import calculate_fitness
+
+
 class Individual:
-    def __init__(self, path, bombs_placed, fitness = 0):
+    def __init__(self, genome, grid_size):
         """
-        Rappresenta un induviduo della popolazione.
-        path è una lista di tuple (row, col, place_bomb) che rappresenta un percorso.
+        Inizializza un nuovo individuo.
+        :param genome:  Stringa di carattreri ( 'u', 'd', 'l', 'r', 'b')
+        :param grid_size: Dimensione della griglia
         """
-        self.path = path
-        self.bombs_placed = bombs_placed
-        self.fitness = fitness
+        self.path = genome
+        self.grid_size = grid_size
+        self.fitness = 0
 
-    def mutate(self, grid, mutation_rate):
-        """Effettua la mutazione casuale sul percorso."""
+    def calculate_fitness(self, grid, player_start, player_goal):
+        """
+        Calcola la fitness di un individuo.
+        :param grid: Griglia di gioco.
+        :param player_start: Posizione di inizio del giocatore.
+        :param player_goal: Posizione dell'obiettivo.
+        :return: fitness dell'individuo.
+        """
+        self.fitness = calculate_fitness(self, grid, player_start, player_goal)
 
-        if random.random() < mutation_rate:
-            index = random.randint(0, len(self.path) - 1)
-            row, col, place_bomb = self.path[index]
 
-            # Cambia casualmente la posizione o la decisione di piazzare una bomb
-            if random.random() < 0.5:
-                self.path[index] = (row, col, not place_bomb)
-            else:
-                new_row, new_col = row + random.choice([-1, 0, 1]), col + random.choice([-1, 0, 1])
-                if grid.is_passable(new_row, new_col):
-                    self.path[index] = (new_row, new_col, place_bomb)
